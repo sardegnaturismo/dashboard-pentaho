07a3df956d77a4d286d1c87d27f2a1c7f8f26a6f
Luca Lusso <llusso@wellnet.it>
2016-05-23 16:02:39 +0200

    FS-29 Supporto al test della Dashboard Sired da parte di Extra - Aggiunto script di avvio macchina Kettle

---
+++ instance.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# START/STOP etl-dasbhoard instance
+#
+
+INSTANCE_ID="i-187a3792"
+
+
+function ec2_status {
+        aws ec2 describe-instance-status --instance-ids $INSTANCE_ID
+}
+
+function ec2_start {
+        echo "START"
+        #aws ec2 start-instances --instance-ids $INSTANCE_ID
+}
+
+function ec2_stop {
+        echo "STOP"
+        #aws ec2 stop-instances --instance-ids $INSTANCE_ID
+}
+
+
+if [ "$1" == "start" ]; then
+        ec2_start
+elif [ "$1" == "stop" ]; then
+        ec2_stop
+elif [ "$1" == "status" ]; then
+        ec2_status
+else
+        ec2_status
+fi
